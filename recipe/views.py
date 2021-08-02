59ec8e0c18577ff357e82d56a10a61b1d26474d5
Rayhan Muktader <rmuktader@gmail.com>
2021-08-01 23:36:22 -0400

    Added recipe image upload feature

--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,17 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import Ingredient, Tag, Recipe
-from recipe.serializers import IngredientSerializer, RecipeDetailSerializer, RecipeSerializer, TagSerializer
+from recipe.serializers import (
+    IngredientSerializer,
+    RecipeDetailSerializer,
+    RecipeImageSerializer,
+    RecipeSerializer,
+    TagSerializer,
+)
 
 
 class BaseRecipeAttrViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
@@ -51,9 +59,22 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return appropriate serializer class"""
         if self.action == "retrieve":
             return RecipeDetailSerializer
+        elif self.action == "upload_image":
+            return RecipeImageSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Save a recipe for the authenticated user"""
         serializer.save(user=self.request.user)
+
+    @action(methods=["POST"], detail=True, url_path="upload-image")
+    def upload_image(self, request, pk=None):
+        """Upload an image to a recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
