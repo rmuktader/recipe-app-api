f681e958b3fba2d989d4fb999943b2fba317c564
Rayhan Muktader <rmuktader@gmail.com>
2021-08-02 23:05:47 -0400

    Delete docker instance after running poetry command and python manage command

--- docker/dc-manage.sh
+++ docker/dc-manage.sh
@@ -1,1 +1,1 @@
-docker-compose run web python manage.py "$@"
+docker-compose run --rm web python manage.py "$@"
--- docker/dc-poetry.sh
+++ docker/dc-poetry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compose run poetry poetry "$@"
+docker-compose run --rm poetry poetry "$@"
 read -p "Do you want to rebuild web image (highly recommended after adding or removing deps)? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
